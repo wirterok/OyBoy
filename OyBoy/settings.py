df4a863a8cb99e9f83829c07dc05853810809a0b
wirter <wirterok@gmail.com>
2024-08-08 22:09:20 +0300

    test print

--- OyBoy/settings.py
+++ OyBoy/settings.py
@@ -16,6 +16,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
